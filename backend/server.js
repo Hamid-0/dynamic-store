082b962937ed1c32891091c5ca55de737f626fdc
Hamid Abolhassani <hamid.abol@outlook.com>
2025-04-20 19:37:38 -0400

    added PUT endpoint for updating products and validation for product ID

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 const app = express();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
@@ -30,6 +31,21 @@ app.post("/api/products", async (req,res)=>{
         res.status(500).json({success:false,message:"Server Error"})
     }
 });
+app.put("/api/products/:id", async (req,res)=>{
+    const {id} = req.params;
+    
+    const product = req.body;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false, message:"Invalid Product Id"})
+    }
+    try {
+        const  updatedProduct = await Product.findByIdAndUpdate(id,product,{new: true});
+        res.status(200).json({success:true, data:updatedProduct});
+    } catch (error) {
+        res.status(500).json({success:false, message:"Server Error"});
+    }
+
+})
 
 app.delete("/api/products/:id", async (req,res)=>{
     const {id} = req.params;
