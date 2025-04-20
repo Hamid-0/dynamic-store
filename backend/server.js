e431bbcf8cd6740ed4e6bdc623b88d1d1e359290
Hamid Abolhassani <hamid.abol@outlook.com>
2025-04-20 19:25:23 -0400

    added mongoDB connection. Added product schema. Added get, delete and post endpoints.

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+const app = express();
+
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.get("/api/products", async(req, res) =>{
+    try {
+        const products = await Product.find({})
+        res.status(200).json({success:"true", data: products})
+    } catch (error) {
+        res.status(500).json({success:false,message:"Server Error"})
+    }
+})
+app.post("/api/products", async (req,res)=>{
+    const product = req.body;// user will send this data
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({success:false, message:"Please provide all fields"});
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save();
+        res.status(201).json({success:true, data:newProduct})
+    } catch (error) {
+        console.error(`Error in Delete Product`,error)
+        res.status(500).json({success:false,message:"Server Error"})
+    }
+});
+
+app.delete("/api/products/:id", async (req,res)=>{
+    const {id} = req.params;
+    
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success:true, message:"Product deleted"})
+
+    } catch (error) {
+        console.error(`Error in Create Product`,error)
+        res.status(500).json({success:false,message:"Product not found"})
+    }
+} )
+
+app.listen(5000, ()=>{
+    connectDB();
+    console.log("server started at port http://localhost:5000")
+})
+
