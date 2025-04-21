6de25169540beaa1e13e70bc224897f518067a6a
Hamid Abolhassani <hamid.abol@outlook.com>
2025-04-21 09:52:24 -0400

    feat: add routing with react-router and create basic page structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
-import { Button } from "@chakra-ui/react"
+import { Box, Button } from "@chakra-ui/react"
+import { Route,Routes } from "react-router-dom"
 function App() {
 
   return (
-    <>
-     <Button>Hello</Button>
-    </>
+    <Box minH={"100vh"}>
+      {/* <Navbar></Navbar> */}
+      <Routes>
+        <Route path="/" element={<HomePage/>}>
+        <Route path="/create" element={<CreatePage />}></Route>
+        </Route>
+      </Routes>
+    </Box>
   )
 }
 
