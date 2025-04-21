2ab266c3e11a90b9b0582ef35d1b0258bf2d0bdd
Hamid Abolhassani <hamid.abol@outlook.com>
2025-04-21 10:24:35 -0400

    feat: add Navbar component with responsive design and icons

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,39 @@
-const Navbar =() =>{
-    return(
-        <div>Navbar</div>
+import { Container, Flex, Text,Link, HStack, Button} from "@chakra-ui/react"
+import { BsFillPlusSquareFill } from "react-icons/bs";
+
+const Navbar = () => {
+    return (
+        <Container maxW={"1140px"} px={4}>
+            <Flex h={16}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+                flexDir={
+                    {
+                        base: "column",
+                        sm: "row"
+                    }
+                }
+            >
+                <Text
+                    fontSize={{ base: "22", sm: "28" }}
+                    fontWeight={"bold"}
+                    textTransform={"uppercase"}
+                    textAlign={"center"}
+                    bgGradient={"linear(to-r, cyan.400, blue.500)"}
+                    bgClip={"text"}
+                >
+                    <Link to={"/"}>Product Store 🛒</Link>
+                </Text>
+                <HStack spacing={2} alignItems={"center"}>
+                <Link to={"/create"}>
+                <Button>
+                    <BsFillPlusSquareFill fontSize={20}/>
+                </Button>
+                </Link>
+
+                </HStack>
+            </Flex>
+        </Container>
     )
 }
 
