9842f5775704d1905724ee25bf087060cae21c0f
Hamid Abolhassani <hamid.abol@outlook.com>
2025-04-21 19:56:05 -0400

    feat: add build commands for production

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,30 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
-import productRoutes from "./routes/product.routes.js"
+import productRoutes from "./routes/product.routes.js";
 import dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
+import path from "path";
+
 dotenv.config();
-
-
 
 const app = express();
 // Enable CORS for all origins
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.use("/api/products", productRoutes)
+app.use("/api/products", productRoutes);
 
-app.listen(PORT, ()=>{
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
     connectDB();
-    console.log(`server started at port http://localhost:${PORT}`)
-})
-
+    console.log(`server started at port http://localhost:${PORT}`);
+});
