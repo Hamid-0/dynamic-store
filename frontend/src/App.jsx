f4492217421f3e314495841aad2e32c1f98555a7
Hamid Abolhassani <hamid.abol@outlook.com>
2025-04-21 10:11:28 -0400

    refactor: clean up App component structure and remove unused Button import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 import { Route,Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import CreatePage from "./pages/CreatePage"
@@ -6,14 +6,16 @@ import Navbar from "./components/Navbar"
 function App() {
 
   return (
-    <Box minH={"100vh"}>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<HomePage/>}>
-        <Route path="/create" element={<CreatePage />}></Route>
-        </Route>
-      </Routes>
-    </Box>
+    
+      <Box minH={"100vh"}>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/create" element={<CreatePage />}/>
+          
+        </Routes>
+      </Box>
+
   )
 }
 
