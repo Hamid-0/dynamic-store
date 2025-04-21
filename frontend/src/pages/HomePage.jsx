d8285f82f0867cee41efa0ada4dfd10bc68d0d4c
Hamid Abolhassani <hamid.abol@outlook.com>
2025-04-21 11:53:03 -0400

    feat: update HomePage component to display current products with styled container

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,20 @@
-const HomePage = () =>{
+import { Container, VStack,Text } from "@chakra-ui/react"
+
+const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <Container maxW={"container.xl"} py={12}>
+            <VStack spacing={8}>
+                <Text
+                    fontSize={"30"}
+                    fontWeight={"bold"}
+                    bgGradient={"linear(to-r, cyan.400, blue.500)"}
+                    bgClip={"text"}
+                    textAlign={"center"}
+                >
+                    Current Products 🚀
+                </Text>
+            </VStack>
+        </Container>
     )
 }
 
