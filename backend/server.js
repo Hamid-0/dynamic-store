0586808bb899a8c9273f590671587baed148d88c
Hamid Abolhassani <hamid.abol@outlook.com>
2025-04-21 20:15:52 -0400

    fix: correct regex pattern in production route handling for better matching

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,9 @@ const __dirname = path.resolve();
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
-
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get(/(.*)/, (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
